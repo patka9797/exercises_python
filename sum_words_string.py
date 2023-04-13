9979bf6526cee38e903716b8b513ceb2e95e3d14
Patrycja Mazur <patrycja.mazur1997@interia.pl>
2023-04-13 18:22:23 +0200

    Testing Counter

--- sum_words_string.py
+++ sum_words_string.py
@@ -1,11 +1,21 @@
+# def sum_word_in_string(string):
+#     counts=dict()
+#     str_split=str.split(string)
+#     for word in str_split:
+#         if word in counts:
+#             counts[word] +=1
+#         else: 
+#             counts[word]=1
+#     return counts
+# print(sum_word_in_string('babka, jajko,babka, kurczak'))
+
+from collections import Counter
+
 def sum_word_in_string(string):
-    counts=dict()
-    str_split=str.split(string)
-    for word in str_split:
-        if word in counts:
-            counts[word] +=1
-        else: 
-            counts[word]=1
-    return counts
-print(sum_word_in_string('babka, jajko,babka, kurczak'))
+
+    print(Counter(string))
+
+print(sum_word_in_string('babka, jajko,babka, kurczak'))   
+
+
 
