12f13ab89e6a3a82bf34f35b9bb80f95fa3f85c5
Patrycja Mazur <patrycja.mazur1997@interia.pl>
2023-04-17 20:01:32 +0200

    Use time module

--- decorator_timer.py
+++ decorator_timer.py
@@ -7,7 +7,6 @@ start=time.time()
 def sum_time(n):
     sum=0
     for i in range(n):
-<<<<<<< HEAD
        sum += i
 
 time.sleep(2)
@@ -20,9 +19,4 @@ elapsed_time = stop - start
 
 print('Execution time:', elapsed_time)
            
-=======
-        for j in range(100000):
-            i*j
-long_time(10)
->>>>>>> 50378364e93c7f3256b22ea999c3792dedc70ed5
 
